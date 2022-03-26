cfee682cbac05fe5a164181a512117e0369a21c9
Craig Robinson <crobinson@forsite.ca>
2022-03-26 07:59:39 -0400

    mar26_2022

--- eVSN.UWP/UWPSQLite.cs
+++ eVSN.UWP/UWPSQLite.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.IsolatedStorage;
 using System.Linq;  
-using System.Text;  
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using eLiDAR.Helpers;
 using eLiDAR.UWP;
+using Nito.AsyncEx;
 using SQLite;  
 using Windows.Storage;  
 using Xamarin.Forms;
@@ -18,7 +20,7 @@ namespace eLiDAR.UWP
     class SqlConnection : ISQLite
     {
         private string path;
-         public SQLiteConnection GetConnection()
+        public SQLiteConnection GetConnection()
         {
             string documentPath = ApplicationData.Current.LocalFolder.Path;
             path = Path.Combine(documentPath, DatabaseHelper.DbFileName);
@@ -29,21 +31,58 @@ namespace eLiDAR.UWP
             {
                 File.Delete(path);
             }
-            _ = CheckFile();
 
-            return new SQLiteConnection(path);
+    //        Task<bool> task = CheckFile();
+       //     bool result = task.GetAwaiter().GetResult();
+
+    //        Task<bool> dofile = CheckFile();
+       //     dofile.Start(); 
+   //         bool result = dofile.Result;
+           // return AsyncHelper.RunSync((c) => CheckFile());
+            var result = AsyncContext.Run(CheckFile);
+            if (result)
+            {
+                return new SQLiteConnection(path);
+            }
+            else { return null; }
         }
-       async Task<bool> CheckFile()
-        {
+            async Task<bool> CheckFile()
+            {
             var storageFile = IsolatedStorageFile.GetUserStoreForApplication();
             if (!storageFile.FileExists(path))
             {
                 // copy storage file; replace if exists.
-                var fileToRead = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///" + DatabaseHelper.DbFileName, UriKind.Absolute));
+                var fileToRead = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/" + DatabaseHelper.DbFileName, UriKind.Absolute));
                 Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
                 StorageFile fileCopy = await fileToRead.CopyAsync(storageFolder, DatabaseHelper.DbFileName, NameCollisionOption.ReplaceExisting);
             }
             return true;
+        }
+    }
+    internal static class AsyncHelper
+    {
+        private static readonly TaskFactory _myTaskFactory = new
+          TaskFactory(CancellationToken.None,
+                      TaskCreationOptions.None,
+                      TaskContinuationOptions.None,
+                      TaskScheduler.Default);
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return AsyncHelper._myTaskFactory
+              .StartNew<Task<TResult>>(func)
+              .Unwrap<TResult>()
+              .GetAwaiter()
+              .GetResult();
+        }
+
+        public static void RunSync(Func<Task> func)
+        {
+            AsyncHelper._myTaskFactory
+              .StartNew<Task>(func)
+              .Unwrap()
+              .GetAwaiter()
+              .GetResult();
         }
     }
 }
